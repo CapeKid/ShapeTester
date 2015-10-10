21e64f6ac7643e8b90bed1e43a133116a2f0ccb4
Seth Micalizzi <srm3t@virginia.edu>
2015-10-10 13:19:40 -0400

    Working one method solution

--- ShapeTesterWebLayer/Scripts/Controllers/HomePageController.js
+++ ShapeTesterWebLayer/Scripts/Controllers/HomePageController.js
@@ -19,95 +19,149 @@
 
 
 
+//TODO: Pass object instead of use array
+//TODO: Use razor, not JS file
+//TODO: Proper namespacing of javascript
+
 //public Canvas object to use in all the functions.
 //Main canvas declaration 
 var canvas;
 var ctx;
-//Width and Height of the canvas
-var WIDTH = 1024;
-var HEIGHT = 740;
+
+// Rectangle number one
+rect1 = {},
+rect1.Width = 0
+rect1.Height = 0
+
+// Rectangle number two
+rect2 = {},
+rect2.Width = 0
+rect2.Height = 0
+
+rectangles = [rect1, rect2]
 
 
-
-// Rectangle array
-rect = {},
-rect.Width = 0
-rect.Height = 0
-//drag= false default to test for the draging
 drag = false;
-// Array to store all the old Shapes drawing details
-var rectStartXArray = new Array();
-var rectStartYArray = new Array();
-var rectWArray = new Array();
-var rectHArray = new Array();
-var rectColor = new Array();
-var Text_ARR = new Array();
+
 
 //Initialize the Canvas and Mouse events for Canvas
 function DrawRectangleOne() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    canvas.addEventListener('mousedown', mouseDown, false);
-    canvas.addEventListener('mouseup', mouseUp, false);
-    canvas.addEventListener('mousemove', mouseMove, false);
     
-    return setInterval(draw, 10);
+    //RemoveMouseListeners(mouseDown2, mouseUp2, mouseMove2);
+    AddMouseListeners(mouseDown, mouseUp, mouseMove, 0);
+    
+    return setInterval(function () { draw(0); }, 10);
+}
+
+function DrawRectangleTwo() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    
+    RemoveMouseListeners(mouseDown, mouseUp, mouseMove);
+    AddMouseListeners(mouseDown2, mouseUp2, mouseMove2);
+
+    return setInterval(draw2, 10);
 }
 
 
+function AddMouseListeners(downEvent, upEvent, moveEvent, rectangleNumber) {
+    canvas.addEventListener('mousedown', function (e) { downEvent(e, rectangleNumber); }, false);
+    canvas.addEventListener('mouseup', function (e) { upEvent(e, rectangleNumber); }, false);
+    canvas.addEventListener('mousemove', function (e) { moveEvent(e, rectangleNumber); }, false);
+}
 
-
+function RemoveMouseListeners(downEvent, upEvent, moveEvent)
+{
+    canvas.removeEventListener('mousedown', downEvent, false);
+    canvas.removeEventListener('mouseup', upEvent, false);
+    canvas.removeEventListener('mousemove', moveEvent, false);
+}
 
 
 //Mouse down event method
-function mouseDown(e) {
-    rect.Width = 0;
-    rect.Height = 0;
-    rect.startX = e.pageX - this.offsetLeft;
-    rect.startY = e.pageY - this.offsetTop;
+function mouseDown(e, rectangleNumber) {
+    rectangles[rectangleNumber].Width = 0;
+    rectangles[rectangleNumber].Height = 0;
+    rectangles[rectangleNumber].startX = e.pageX - canvas.offsetLeft;
+    rectangles[rectangleNumber].startY = e.pageY - canvas.offsetTop;
     
     drag = true;
 }
 //Mouse UP event Method
-function mouseUp() {
-    //rectStartXArray[rectStartXArray.length] = rect.startX;
-    //rectStartYArray[rectStartYArray.length] = rect.startY;
-    //rectWArray[rectWArray.length] = rect.w;
-    //rectHArray[rectHArray.length] = rect.h;
-    
-    //rectColor[rectColor.length] = "#444444";
-    
+function mouseUp(e, rectangle) {
     drag = false;
 }
 
 //mouse Move Event method
-function mouseMove(e) {
+function mouseMove(e, rectangleNumber) {
+
     if (drag) {
-        rect.Width = (e.pageX - this.offsetLeft) - rect.startX;
-        rect.Height = (e.pageY - this.offsetTop) - rect.startY;
         
-        draw();
+        rectangles[rectangleNumber].Width = (e.pageX - canvas.offsetLeft) - rectangles[rectangleNumber].startX;
+        //console.log(rectangles[rectangleNumber].Width);
+        rectangles[rectangleNumber].Height = (e.pageY - canvas.offsetTop) - rectangles[rectangleNumber].startY;
+        
+        draw(rectangleNumber);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 }
 
 
-
-
-
 //Draw all Shaps,Text and add images 
-function draw() {
+function draw(rectangleNumber) {
+    //alert("!");
     ctx.beginPath();
-    ctx.fillStyle = "#444444";
-    ctx.rect(rect.startX, rect.startY, rect.Width, rect.Height);
-            
+    ctx.fillStyle = "#FF0000";
+    ctx.rect(rectangles[rectangleNumber].startX, rectangles[rectangleNumber].startY, rectangles[rectangleNumber].Width, rectangles[rectangleNumber].Height);
+
     ctx.fill();
 }
 
 
 
+//Mouse down event method
+function mouseDown2(e) {
+    rect2.Width = 0;
+    rect2.Height = 0;
+    rect2.startX = e.pageX - this.offsetLeft;
+    rect2.startY = e.pageY - this.offsetTop;
+
+    drag = true;
+}
+//Mouse UP event Method
+function mouseUp2() {
+    drag = false;
+}
+
+//mouse Move Event method
+function mouseMove2(e) {
+    if (drag) {
+        rect2.Width = (e.pageX - this.offsetLeft) - rect2.startX;
+        rect2.Height = (e.pageY - this.offsetTop) - rect2.startY;
+
+        draw2();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+
+//Draw all Shaps,Text and add images 
+function draw2() {
+    ctx.beginPath();
+    ctx.fillStyle = "#0000FF";
+    ctx.rect(rect2.startX, rect2.startY, rect2.Width, rect2.Height);
+
+    ctx.fill();
+}
+
+
+
+
+
 //save as image file 
 function SaveRectangle() { 
-    jQuery.post("/Home/RectOne", { X: rect.startX, Y: rect.startY, Height: rect.Height, Width: rect.Width }, function (data) {
+    jQuery.post("/Home/RectOne", { X: rect1.startX, Y: rect1.startY, Height: rect1.Height, Width: rect1.Width }, function (data) {
     });
 }
